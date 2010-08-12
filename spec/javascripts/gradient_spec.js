e6f5889a49a3a5a43605cef2aab43653f958bfe9
Roy Kolak <roy.kolak@gmail.com>
2010-08-12 10:22:28 -0500

    updated specs for ie gradient change

--- spec/javascripts/gradient_spec.js
+++ spec/javascripts/gradient_spec.js
@@ -1,10 +1,18 @@
 describe("gradient", function() {
-  it("sets css gradient properties for all browsers on the passed element using the passed colors", function() {
+  beforeEach(function() {
     spyOn($.fn, 'css');
+  });
+  
+  it("sets css gradient properties for all browsers on the passed element using the passed colors", function() {
     gradient({}, '#CCC', '#DDD');
     expect($.fn.css).wasCalledWith('background', 'linear-gradient(left top, #CCC, #DDD)'); 
     expect($.fn.css).wasCalledWith('background', '-webkit-gradient(linear, 0% 0%, 0% 100%, from(#CCC), to(#DDD))');
     expect($.fn.css).wasCalledWith('background', '-moz-linear-gradient(center top, #CCC, #DDD)');
-    expect($.fn.css).wasCalledWith('filter', "progid:DXImageTransform.Microsoft.gradient(startColorstr='#CCC', endColorstr='#DDD')");
+  });
+  
+  it("sets a css background color when the browser is internet explorer", function() {
+    $.browser.msie = true;
+    gradient({}, '#CCC', '#DDD');
+    expect($.fn.css).wasCalledWith('background', '#CCC');
   });
 });
